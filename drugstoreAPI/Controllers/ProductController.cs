7944e03a16d9c34ce07781dd4def436ec34f1156
Valentina <feruereru@gmail.com>
2020-05-25 21:35:13 +0000

    Showing products in json from database

---
+++ drugstoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using drugstoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace drugstoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            using (var context = new DotNetContext() )
+            {
+              // Get all the products from the database.
+              return context.Product.ToList();
+            }
+        }
+    }
+}
