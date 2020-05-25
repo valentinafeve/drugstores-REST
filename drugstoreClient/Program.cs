9e509ec47b17424f12022b29c77e5272c23da5f8
Valentina <feruereru@gmail.com>
2020-05-25 21:04:58 +0000

    GRPC + client

---
+++ drugstoreClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace drugstoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
